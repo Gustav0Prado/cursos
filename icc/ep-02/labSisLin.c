6ea3f8b836008d3350d36fe7e06d71885a5971c1
Gustavo P <guprado.silva@gmail.com>
2022-11-17 12:24:33 -0300

    [fix] flag Wall no makefile [add] interface de print principal

--- icc/ep-02/labSisLin.c
+++ icc/ep-02/labSisLin.c
@@ -12,8 +12,7 @@
 #include "sislin.h"
 #include "Metodos.h"
 
-#define QTD_MAT 1
-//#define QTD_MAT 6
+#define QTD_MAT 6
 
 /*!
   \brief Copia valores de uma matriz para outra
@@ -36,14 +35,18 @@ int main ()
   // inicializa gerador de números aleatóreos
   srand(202202);
   
-  //int TAM_MATRIZES[QTD_MAT] = {10, 30, 50, 128, 256, 512};
-  int TAM_MATRIZES[QTD_MAT] = {3};
+  int TAM_MATRIZES[QTD_MAT] = {10, 30, 50, 128, 256, 512};
+
 
   SistLinear_t *SL1, *SL2;
   real_t *x1, *x2, *x3;
-  double tTotal = 0.0;
+  double tEGP = 0.0, tGS = tEGP, tREF = tEGP;
 
   printf("Diagonal Dominante: \n");
+  printf("   n   |   t_egp   | normaResiduo_egp |    t_gs    |   it_gs   |");
+  printf(" normaResiduo_gs |   t_ref   |   it_ref   | normaResiduo_ref|\n");
+  printf("-------------------------------------------------------------");
+  printf("--------------------------------------------------------------\n");
   for(int i = 0; i < QTD_MAT; i++){
     int tam = TAM_MATRIZES[i];
 
@@ -57,15 +60,11 @@ int main ()
     iniSisLin(SL1, diagDominante, 32.0);
     copiaMatriz(SL2, SL1, tam);
 
-    printf("   Tamanho: %d\n", tam);
-
     /* eliminacao de gauss */
-    eliminacaoGauss(SL1, x1, &tTotal);
-    printf("   Eliminação Gauss:   %g\n", tTotal);
+    eliminacaoGauss(SL1, x1, &tEGP);
 
     /* Gauss-Seidel */
-    int itGS = gaussSeidel(SL2, x2, ERRO, &tTotal);
-    printf("   Gauss-Seidel:       %g, %2d iteracoes\n", tTotal, itGS);
+    int itGS = gaussSeidel(SL2, x2, ERRO, &tGS);
 
     /* copia x1 pra x3 */
     for(int i = 0; i < tam; i++){
@@ -73,13 +72,96 @@ int main ()
     }
 
     /* refinamento */
-    int itR = refinamento(SL2, x3, ERRO, &tTotal);
-    printf("   Refinamento:        %g, %2d iteracoes\n", tTotal, itR);
-    printf("   -------------------------------------------\n");
+    int itREF = refinamento(SL2, x3, ERRO, &tREF);
 
-    // prnVetor(x1, tam);
-    // prnVetor(x2, tam);
-    // prnVetor(x3, tam);
+    printf("%4d   |%10g |   %10g     | %10g |   %4d    |   %10g    |", tam, tEGP, 0.015472, tGS, itGS, 0.015472);
+    printf("%10g |    %4d    |   %10g     \n", tREF, itREF, 0.015472);
+
+    /* Desaloca estruturas */
+    liberaSisLin(SL1);
+    liberaSisLin(SL2);
+    free(x1);
+    free(x2);
+    free(x3);
+  }
+
+  printf("\n\nGenerica: \n");
+  printf("   n   |   t_egp   | normaResiduo_egp |    t_gs    |   it_gs   |");
+  printf(" normaResiduo_gs |   t_ref   |   it_ref   | normaResiduo_ref|\n");
+  printf("-------------------------------------------------------------");
+  printf("--------------------------------------------------------------\n");
+  for(int i = 0; i < QTD_MAT; i++){
+    int tam = TAM_MATRIZES[i];
+
+    SL1 = alocaSisLin(tam);
+    SL2 = alocaSisLin(tam);
+    x1 = malloc(sizeof(real_t)*tam);
+    x2 = malloc(sizeof(real_t)*tam);
+    x3 = malloc(sizeof(real_t)*tam);
+
+    //if SL -> testa ponteiro
+    iniSisLin(SL1, generico, 32.0);
+    copiaMatriz(SL2, SL1, tam);
+
+    /* eliminacao de gauss */
+    eliminacaoGauss(SL1, x1, &tEGP);
+
+    /* Gauss-Seidel */
+    int itGS = gaussSeidel(SL2, x2, ERRO, &tGS);
+
+    /* copia x1 pra x3 */
+    for(int i = 0; i < tam; i++){
+      x3[i] = x1[i];
+    }
+
+    /* refinamento */
+    int itREF = refinamento(SL2, x3, ERRO, &tREF);
+
+    printf("%4d   |%10g |   %10g     | %10g |   %4d    |   %10g    |", tam, tEGP, 0.015472, tGS, itGS, 0.015472);
+    printf("%10g |    %4d    |   %10g     \n", tREF, itREF, 0.015472);
+
+    /* Desaloca estruturas */
+    liberaSisLin(SL1);
+    liberaSisLin(SL2);
+    free(x1);
+    free(x2);
+    free(x3);
+  }
+
+  printf("\n\nHilbert: \n");
+  printf("   n   |   t_egp   | normaResiduo_egp |    t_gs    |   it_gs   |");
+  printf(" normaResiduo_gs |   t_ref   |   it_ref   | normaResiduo_ref|\n");
+  printf("-------------------------------------------------------------");
+  printf("--------------------------------------------------------------\n");
+  for(int i = 0; i < QTD_MAT; i++){
+    int tam = TAM_MATRIZES[i];
+
+    SL1 = alocaSisLin(tam);
+    SL2 = alocaSisLin(tam);
+    x1 = malloc(sizeof(real_t)*tam);
+    x2 = malloc(sizeof(real_t)*tam);
+    x3 = malloc(sizeof(real_t)*tam);
+
+    //if SL -> testa ponteiro
+    iniSisLin(SL1, hilbert, 32.0);
+    copiaMatriz(SL2, SL1, tam);
+
+    /* eliminacao de gauss */
+    eliminacaoGauss(SL1, x1, &tEGP);
+
+    /* Gauss-Seidel */
+    int itGS = gaussSeidel(SL2, x2, ERRO, &tGS);
+
+    /* copia x1 pra x3 */
+    for(int i = 0; i < tam; i++){
+      x3[i] = x1[i];
+    }
+
+    /* refinamento */
+    int itREF = refinamento(SL2, x3, ERRO, &tREF);
+
+    printf("%4d   |%10g |   %10g     | %10g |   %4d    |   %10g    |", tam, tEGP, 0.015472, tGS, itGS, 0.015472);
+    printf("%10g |    %4d    |   %10g     \n", tREF, itREF, 0.015472);
 
     /* Desaloca estruturas */
     liberaSisLin(SL1);
