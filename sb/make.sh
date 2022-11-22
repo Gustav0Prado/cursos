0042f9c06f992c98e4834125d7a5ee82d69ae39c
Gustavo P <guprado.silva@gmail.com>
2022-11-22 11:21:25 -0300

    [add] opcao clean no make.sh [add] for para compilar varios arquivos

--- sb/make.sh
+++ sb/make.sh
@@ -1,4 +1,15 @@
-as $1.s -o $1.o
-ld $1.o -o $1
-./$1
-echo $?
+#!/bin/bash
+
+shopt -s extglob
+
+for i in "$@"
+do
+   if [ "$i" = "--clean" -o "$i" = "-c" ]; then
+      rm -v !(*.s|*.pdf|*.sh|.gitignore)
+   else
+      as $i.s -o $i.o
+      ld $i.o -o $i
+      ./$i
+      echo $?
+   fi
+done
