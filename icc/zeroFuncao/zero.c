60ab5fe8710eaeb387ce26b524dd9541da39a71f
Gustavo P <guprado.silva@gmail.com>
2022-11-25 10:37:46 -0300

    [add] contador de iteracoes

--- icc/zeroFuncao/zero.c
+++ icc/zeroFuncao/zero.c
@@ -8,12 +8,13 @@ double func(double a)
    return pow(a, 2) - 1;
 }
 
+/* x - ( f(x)/f'(x) ) */
 double phi(double a)
 {
    return ( a - (func(a)/2*a) );
 }
 
-double bissecao( double xl, double xu, double f(double a), double epsilon )
+double bissecao( double xl, double xu, double f(double a), double epsilon, int *iter )
 {
    double xm_new, xm_old;
 
@@ -40,17 +41,17 @@ double bissecao( double xl, double xu, double f(double a), double epsilon )
       else{
          return xm_new;
       }
+      ++(*iter);
    } while(fabs( (xm_new - xm_old)/(xm_new) )*100 > epsilon);
 
    return xm_new;
 }
 
 double iteracao(double x0, double f(double x), double phi(double x), 
-                double epsilon_x, double epsilon_f, int max_iter)
+                double epsilon_x, double epsilon_f, int max_iter, int *iter)
 {
    double x_new = x0, x_old;
    int crit1, crit2, crit3;
-   int iter = 1;
 
    do
    {
@@ -59,8 +60,8 @@ double iteracao(double x0, double f(double x), double phi(double x),
 
       crit1 = fabs(x_new - x_old) < epsilon_x;
       crit2 = fabs(f(x_new)) < epsilon_f;
-      crit3 = (iter == max_iter);
-      ++iter;
+      crit3 = (*iter == max_iter);
+      ++(*iter);
    } while(!crit1 && !crit2 && !crit3);
 
    return x_new;
@@ -68,6 +69,7 @@ double iteracao(double x0, double f(double x), double phi(double x),
 
 int main(){
    double xl, xu, x0;
+   int iter1 = 1, iter2 = 1;
 
    printf("Insira o limite esquerdo: ");
    scanf("%lf", &xl);
@@ -80,6 +82,8 @@ int main(){
    void *f = &func;
    void *p = &phi;
 
-   printf("Por bissecao: \n\tZero em: %g\n", bissecao(xl, xu, f, erro));
-   printf("Por iteração: \n\tZero em: %g\n", iteracao(x0, f, p, 1e-10, 1e-10, 100));
+   printf("Por bissecao: \n\tZero em: %g\n", bissecao(xl, xu, f, erro, &iter1));
+   printf("\t%d iterações\n", iter1);
+   printf("Por iteração: \n\tZero em: %g\n", iteracao(x0, f, p, 1e-10, 1e-10, 100, &iter2));
+   printf("\t%d iterações\n", iter2);
 }
