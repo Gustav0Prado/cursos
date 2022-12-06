7511b9d9b4b4306ffaef7b4121bfb27d8521a929
Gustavo P <guprado.silva@gmail.com>
2022-12-06 11:22:18 -0300

    [add] gitignore ep-04

--- icc/ep-04/ZeroFuncao.c
+++ icc/ep-04/ZeroFuncao.c
@@ -7,24 +7,39 @@
 double newtonRaphson (Polinomio p, double x0, double eps,
 		   int *it, double *raiz)
 {
+	double erro;
+	do{
 
+	} while(erro > eps && *it < MAXIT);
 }
 
 
 double secante (Polinomio p, double x0, double x1, double eps,
 	     int *it, double *raiz)
 {
+	double erro;
+	do{
 
+	} while(erro > eps && *it < MAXIT);
 }
 
 
 void calcPolinomio_rapido(Polinomio p, double x, double *px, double *dpx)
 {
-
+	*px = 0;
+	*dpx = 0;
+	for(int i = p.grau; i > 0; --i){
+		*px = (*px)*x + p.p[i];
+		*dpx = (*dpx)*x + *px;
+	}
+	*px = *px*x + p.p[0];
 }
 
 
 void calcPolinomio_lento(Polinomio p, double x, double *px, double *dpx)
 {
-
+	for(int i = p.grau; i >= 0; --i){
+		*px += p.p[i] * pow(x, i);
+		*dpx += i*p.p[i] * pow(x, i-1);
+	}
 }
--- icc/ep-04/labZero.c
+++ icc/ep-04/labZero.c
@@ -6,11 +6,23 @@
 
 int main ()
 {
+  Polinomio p;
+  double x0, x1;
 
+  printf("Grau do Polinomio: ");
+  scanf("%d", &p.grau);
+  p.p = malloc(sizeof(double)*p.grau+1);
 
+  for(int i = p.grau; i >=0 ; --i){
+    printf("Valor de a%d: ", i);
+    scanf("%lg", &p.p[i]);
+  }
 
+  printf("x0: ");
+  scanf("%lg", &x0);
 
-
+  printf("x1: ");
+  scanf("%lg", &x1);
 
   return 0;
 }
