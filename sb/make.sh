42d9ced703d1f999d88fe5f1f69bcf22aaf5fd84
Gustavo <guprado.silva@gmail.com>
2022-12-13 14:48:13 -0300

    [add] programa imprime tamanho das secoes data e text [add] -g na compilacao assembly

--- sb/make.sh
+++ sb/make.sh
@@ -10,13 +10,13 @@ i=(${args[0]})
 if [ "$last" = "--clean" -o "$last" = "-c" ]; then
    rm -v !(*.s|*.pdf|*.sh|.gitignore)
 elif [ "$last" = "--printf" -o "$last" = "-p" ]; then
-   as $i.s -o $i.o
+   as $i.s -o $i.o -g
    ld $i.o -o $i -dynamic-linker /lib/x86_64-linux-gnu/ld-linux-x86-64.so.2 \
    /usr/lib/x86_64-linux-gnu/crt1.o /usr/lib/x86_64-linux-gnu/crti.o \
    /usr/lib/x86_64-linux-gnu/crtn.o -lc
    ./$i
 else
-   as $i.s -o $i.o
+   as $i.s -o $i.o -g
    ld $i.o -o $i
    ./$i
    echo $?
--- sb/textdata.s
+++ sb/textdata.s
@@ -1,14 +1,29 @@
 .section .data
     A: .quad 0
+    str1: .string ".data %d bytes\n"
+    str2: .string ".text %d bytes\n"
     B: .quad 0
-    C: .quad 0
 .section .text
-.globl _start
-
-# main:
-_start:
+.globl main
+main:
+    _c:
     pushq %rbp
     movq %rsp, %rbp
-    movq A, %rdi
+    movq $A, %r11
+    movq $B, %rbx
+    subq %r11, %rbx
+    movq $str1, %rdi
+    movq %r11, %rsi
+    call printf
+
+    movq $_c, %r11
+    movq $_d, %rbx
+    subq %r11, %rbx
+    movq $str2, %rdi
+    movq %r11, %rsi
+    call printf
+
+    movq $0, %rdi
     movq $60, %rax
     syscall
+    _d:
