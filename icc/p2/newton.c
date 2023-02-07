3182d79d98e9e53ddb60402e33ff4e41dd459af6
Gustavo P <guprado.silva@gmail.com>
2023-02-07 11:16:20 -0300

    [add] metodo do polinomio de newton

--- icc/p2/newton.c
+++ icc/p2/newton.c
@@ -25,5 +25,39 @@ int main(){
    printf("Insira o valor de x a ser interpolado: ");
    scanf("%lg", &xint);
 
+   //matriz de diferencas divididas
+   //linha 1 = f(xi)
+   for(int i = 0; i < n; ++i){
+      tab[0][i] = y[i];
+   }
+   //constroi as outras linhas
+   for(int i = 1; i < n; ++i){
+      for(int j = 0; j < n-i; ++j){
+         tab[i][j] = (tab[i-1][j+1] - tab[i-1][j]) / (x[j+i]-x[j]);
+      }
+   }
 
+   printf("\n");
+   for(int i = 0; i < n; ++i){
+      for(int j = 0; j < n-i; ++j){
+         printf("%15.7f ", tab[i][j]);
+      }
+      printf("\n");
+   }
+   printf("\n");
+
+   double soma = 0.0;
+   //calcula valor do polinomio
+   for(int i = 0; i < n; ++i){
+      double d = tab[i][0];
+      double mult = 1.0;
+
+      for(int j = 0; j < i; ++j){
+         mult *= (xint - x[j]);
+      }
+
+      soma += d*mult;
+   }
+
+   printf("P(x) = %g\n", soma);
 }
