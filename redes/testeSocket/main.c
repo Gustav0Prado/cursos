4f8f5e686e7ab2d7ad48d77b5ac7e9ab8aa89ed6
Gustavo Prado <guprado.silva@gmail.com>
2023-05-06 17:14:02 -0300

    [add] teste de Socket manda Hello World pela rede

--- redes/testeSocket/main.c
+++ redes/testeSocket/main.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/socket.h>
 #include "socket.h"
 
 int main(){
-   printf("%d\n", ConexaoRawSocket("aa"));
+   int papel;
+   unsigned char msg[256];
+
+   int socket = ConexaoRawSocket("lo");
+
+   printf("0 - Cliente\n1 - Servidor\n");
+   scanf("%d", &papel);
+
+   if(papel){
+      while(1){
+         if(recv(socket, msg, sizeof(msg), 0) >= 0){
+            printf("%s\n", msg);
+         }
+      }
+   }
+   else{
+      strncpy(msg, "Hello, World!", 15);
+      send(socket, msg, sizeof(msg), 0);
+   }
 }
