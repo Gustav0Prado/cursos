b3e7e99694d01dafed2efba604c556d519b201a3
Gustavo P <guprado.silva@gmail.com>
2023-06-01 11:27:46 -0300

    [fix] Teste com var passada por referencia

--- compiladores/ProjetoBase/teste1.pas
+++ compiladores/ProjetoBase/teste1.pas
@@ -1,12 +1,13 @@
 program proc2 (input, output);
 var x, y: integer;
-   procedure p(t: integer; var u :integer);
+   procedure p(t:integer; var u :integer);
    begin
       t := t + 10;
+      write(t);
       u := u + 10;
    end;
 begin
    read(x, y);
    p(x-1, y);
-   write(x, y);
+   write(y);
 end.
