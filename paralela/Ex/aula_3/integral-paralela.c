f6fb5cb9c88de1fc66428fca2f867092dc4dc239
Gustavo <guprado.silva@gmail.com>
2023-09-13 18:57:37 -0300

    [add] Mandel e aula 3 + [mod] Separa em pastas

---
+++ paralela/Ex/aula_3/integral-paralela.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+static long num_steps = 1000000 * 1;
+double step;
+
+int main() {
+   int i; double x, pi, sum = 0.0;
+
+   step = 1.0/(double) num_steps;
+   #pragma omp parallel for reduction (+:sum) private(x)
+      for(i = 0; i < num_steps; ++i) {
+         x = (i + 0.5) * step; // Largura do retangulo
+         sum = sum + 4.0 / (1.0 + x*x); // Sum += Area do retangulo
+      }
+      pi = step * sum;
+
+   printf("Resultado Sequencial: %.15f\n", pi);
+}
