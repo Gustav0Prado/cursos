81a26d09fb8496ab681b8a5cc4604a4067321362
Gustavo P <guprado.silva@gmail.com>
2024-05-02 14:35:55 -0300

    [fix] Corrige leitura de tuplas

--- top-ia/t3/solver.py
+++ top-ia/t3/solver.py
@@ -21,10 +21,20 @@ class Restricao:
 
    def __str__(self):
       saida  = "\tTipo da restricao = " + str(self.tipo) + "\tTamanho do escopo = " + str(self.tamanho_escopo) + "\n\tEscopo = " + str(self.escopo)
-      saida += "\n\tTamanho do tuplas = " + str(self.tamanho_tuplas) + "\n\tTuplas = " + str(self.tuplas)
+      saida += "\n\tQuantidade de tuplas = " + str(self.tamanho_tuplas) + "\n\tTuplas = " + str(self.tuplas)
       return saida
 
-def split(lista, tam):
+
+def split(tam, lista):
+   """Particiona uma lista em várias listas menores de tamanho tam
+
+   Args:
+       tam (int):    Tamanho das listas de saída
+       lista (list): Lista de entrada
+
+   Returns:
+       res: Lista com as listas menores particionadas 
+   """
    res = []
    while lista != []:
       l = []
@@ -39,25 +49,28 @@ restricoes_problema = []
 num_var  = 0
 num_rest = 0
 
+# Leitura do arquivo de entrada
 with open(sys.argv[1]) as entrada:
+   # Le variaveis
    num_var = int(entrada.readline())
    for i in range(num_var):
       linha = entrada.readline()
-      res = [eval(i) for i in linha.strip('\n').split(' ')]
+      res = [eval(i) for i in linha.strip('\n').split(' ')] # Converte a linha dada na entrada para uma lista de inteiros
       
       dominio_xn = Dominio(res[0], res[1:])
       dominio_problema.append(dominio_xn)
 
+   # Le as restrições
    num_rest = int(entrada.readline())
    for i in range(num_rest):
       linha_tipo   = entrada.readline()
       linha_escopo = entrada.readline()
       linha_tuplas = entrada.readline()
       
-      res_escopo = [eval(i) for i in linha_escopo.strip('\n').split(' ')]
-      res_tuplas = [eval(i) for i in linha_tuplas.strip('\n').split(' ')]
+      res_escopo = [eval(i) for i in linha_escopo.strip('\n').split(' ')] # Converte a linha dada na entrada para uma lista de inteiros
+      res_tuplas = [eval(i) for i in linha_tuplas.strip('\n').split(' ')] # Converte a linha dada na entrada para uma lista de inteiros
       
-      restricao_n = Restricao(linha_tipo, res_escopo[0], res_escopo[1:], res_tuplas[0], split(res_tuplas[1:], res_tuplas[0]))
+      restricao_n = Restricao(linha_tipo, res_escopo[0], res_escopo[1:], res_tuplas[0], split(res_escopo[0], res_tuplas[1:]))
       restricoes_problema.append(restricao_n)
 
 for i in range(num_var):
