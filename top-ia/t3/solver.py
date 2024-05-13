73dc62e24375627b621ba0f76db01349b10c9371
Gustavo P <guprado.silva@gmail.com>
2024-05-13 17:02:09 -0300

    [add] Inicia funcao de verificar restricoes

--- top-ia/t3/solver.py
+++ top-ia/t3/solver.py
@@ -15,15 +15,23 @@ def reset_validos(dom: Dominio):
    dom.valido = [1]*dom.tamanho
 
 # Verifica se alguma das restricoes nao esta sendo seguida
-def verifica_restricoes():
-   return True
+def verifica_restricoes(prob: Problema, valores: list):
+   # TODO: pular verificacao na primeira vez
+   for res in prob.restricoes_problema:
+      for tupla in res.tuplas:
+         for i in range(res.tamanho_tuplas):
+            print(f'escopo: {res.escopo}')
+            print(f'tupla: {tupla}')
+            print(f'valores: {valores}')
+            if valores[res.escopo[i]] != tupla[i]:
+               return False
 
 def backtrack(prob: Problema):   
    valores = [0]*prob.num_var  # Cria uma valoracao inicial zerada
-   i = 1
+   i = 0
 
-   while (i >= 1) and (i <= prob.num_var):
-      if not verifica_restricoes:
+   while (i >= 0) and (i < prob.num_var):
+      if not verifica_restricoes(prob, valores):
          reset_validos(prob.dominio_problema[i])
          i = i - 1
       else:
