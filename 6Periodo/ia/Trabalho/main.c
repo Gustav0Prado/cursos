b0e4dd31972da1a4d97c9b586e2dc235c4887680
gustavops <guprado.silva@gmail.com>
2024-10-23 21:32:05 -0300

    [add] Inicio trabalho de IA

---
+++ 6Periodo/ia/Trabalho/main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int n, m;
+    int **game;
+
+    scanf("%i", &n);
+    scanf("%i", &m);
+
+    /* Aloca matriz */
+    game = malloc(sizeof(int*)*n);
+    for (int i = 0; i < n; ++i) {
+        game[i] = malloc(sizeof(int) * m);
+    }
+
+    /* Le toda matriz */
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            scanf("%i ", &game[i][j]);
+        }
+    }
+
+    /* Imprime matriz */
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            printf("%d ", game[i][j]);
+        }
+        printf("\n");
+    }
+
+    /* Libera estruturas usadas */
+    for (int i = 0; i < n; ++i) {
+        free(game[i]);
+    }
+    free(game);
+}
